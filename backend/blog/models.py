20bf3771be44177100baa0fdd7791f53287d9db9
jorgeromeroc <jromerocontreras@icloud.com>
2023-01-15 16:11:20 -0300

    Modelos de blog creados y app blog

---
+++ backend/blog/models.py
@@ -0,0 +1,35 @@
+from django.contrib.admin import options
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+
+
+# Donde guardaremos las imagenes
+def user_directory_path(instance, filename):
+    return 'blog/{0}/{1}'.format(instance.title, filename)
+
+class Post(models.Model):
+
+    class PostObjects(models.Model):
+        def get_queryset(self):
+            return super().get_queryset() .filter(status='published')
+
+    options = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+
+    title = models.CharField(max_length=250)
+    thumbnail = models.ImageField(upload_to=user_directory_path, blank=True, null=True)
+    excerpt = models.TextField(null=True)
+    content = models.TextField()
+    slug = models.SlugField(max_length=250, unique_for_date='published', null=False, unique=True)
+    published = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_user')
+
+    status = models.CharField(max_length=10, choices=options, default='draft')
+    objects = models.Manager()
+    postobjects = PostObjects()
+
+    class Meta:
+        ordering = ('-published',)
